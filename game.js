ec5bbb3e4d22f5fa7523fb4cfae80f3c6d58b19d
Eric Fraze <magnilucent@gmail.com>
2014-11-03 07:32:31 -0500

    Added enemy AI / Fixed distance function

--- game.js
+++ game.js
@@ -12,8 +12,10 @@ var breadcrumbs;
 var viewport;
 // The input manager
 var input;
+// List of all teams and the ships inside the teams
+var teams;
 
-
+var mousePos;
 
 // Start the game
 initGame();
@@ -23,6 +25,7 @@ initGame();
 function initGame() {
 	steps = 0;
 	ships = new Array();
+	teams = new Array();
 	breadcrumbs = new Array();
 
 	// Create the input
@@ -33,6 +36,9 @@ function initGame() {
 							document.getElementById("myCanvas").offsetHeight);
 	// Spawn player ship
 	new Ship(1000, 1000, 0, 1, "#3FC380", true);
+
+	// Spawn enemy ship
+	new Ship(950, 1200, 1, 1, "#FF0000", false);
 
 	// Set the canvas to the correct size
 	c.setAttribute('width', viewport.width);
@@ -56,9 +62,9 @@ function update() {
 
 	// Update the ships
 	ships.forEach ( function(ship) {
+		ship.update();
 		if (ship.isPlayer) {
 			// Center the viewport on the player
-			ship.update();
 			viewport.focus(ship);
 		}
 	});
@@ -118,6 +124,14 @@ function Ship (x, y, team, size, color, isPlayer) {
 	this.maxSpeed = 10;
 	this.minSpeed = 3;
 
+	this.turnSpeed = 5;
+
+	// Used by AI. Whatever it is trying to kill.
+	this.focus;
+
+	// AI takes 5 steps to become aware of a change
+	this.reactionTime = 5;
+
 	// Used to center the ship when it is being drawn
 	this.center = 0;
 	if (this.size > 1) {
@@ -125,7 +139,15 @@ function Ship (x, y, team, size, color, isPlayer) {
 	}
 
 	// Add the ship to the list
-	ships.push (this);
+	ships.push(this);
+
+	// Add the ship to the team list
+	if (typeof teams.team == 'undefined') {
+		teams[team] = new Array(this);
+		console.log("Created team " + team);
+	}else{
+		teams[team].push(this);
+	}
 
 	// X relative to the viewport
 	this.relativeX = function () {
@@ -148,10 +170,57 @@ function Ship (x, y, team, size, color, isPlayer) {
 				this.speed -= 0.4;
 
 			if (input.keyboardCheck("A"))
-				this.direction -= 0.15;
+				this.direction -= this.turnSpeed;
 
 			if (input.keyboardCheck("D"))
-				this.direction += 0.15;
+				this.direction += this.turnSpeed;
+		} else {
+			// AI handling
+
+			// Find enemy if needed
+			if (typeof this.focus == 'undefined') {
+				this.focus = this.findNearestEnemy();
+			}
+
+			// Handle enemy fighting
+			if (typeof this.focus !== 'undefined') {
+				// Angle to enemy ship
+				var angle = getAngle(this.x, this.y, this.focus.x, this.focus.y);
+
+				// Differance between the ship's direction and angle to enemy
+				var difference =  this.direction - angle;
+
+				// Find shortest path to correct angle
+				if (Math.abs(difference) > 180) {
+					if (this.direction > angle) {
+						difference = -1 * ((360 - this.direction) + angle);
+					} else {
+						difference = (360 - angle) + this.direction;
+					}
+				}
+
+				// Activated when ship is moving to assault enemy ship
+				if (getDistance(this.x, this.y, this.focus.x, this.focus.y) < 100 && Math.abs(difference) < 30) {
+					// Slow down behind the enemy and maych speed
+					if (this.speed > this.focus.speed) {
+						this.speed -= 0.2;
+						if (this.speed < this.focus.speed)
+							this.speed = this.focus.speed;
+					}
+				}else{
+					// Boast towards enemy
+					this.speed += 0.3;
+				}
+
+				// Activated when enemy ship is close BEHIND ship
+				if ( Math.abs(difference) > 120 && getDistance(this.x, this.y, this.focus.x, this.focus.y) < 150) {
+					// Causes the ship to follow a S shaped path
+					this.direction += this.turnSpeed * sign(difference);
+				}else if ( Math.abs(difference) > 10 ) {
+					// Turn towards enemy ship
+					this.direction -= this.turnSpeed * sign(difference);
+				}
+			}
 		}
 
 		// Clamp speed
@@ -161,15 +230,43 @@ function Ship (x, y, team, size, color, isPlayer) {
 			this.speed = this.maxSpeed;
 		}
 
+		// Fix direction values
+		if (this.direction >= 360) {
+			this.direction = 360 - this.direction;
+		}else if (this.direction < 0) {
+			this.direction = 360 + this.direction;
+		}
+
 		// Move
-		this.x += Math.cos(this.direction)*this.speed;
-		this.y += Math.sin(this.direction)*this.speed;
+		this.x += Math.cos(this.direction * (Math.PI/180) )*this.speed;
+		this.y += Math.sin(this.direction * (Math.PI/180) )*this.speed;
 
 		// Make a breadcrumb trail
 		if ( (steps / (3) ) == Math.round(steps / (3) )) {
 			new Breadcrumb(this.x, this.y, this.size, "white", 2*30);
 		}
 	};
+
+
+	this.findNearestEnemy = function () {
+		var closest ;
+		for (i = 0; i < teams.length; i++) {
+			if (i == team) {
+				continue;
+			}else{
+				teams[i].forEach ( function(enemy) {
+					if ( typeof closest == 'undefined' ){
+						closest = enemy;
+					} else if ( getDistance(this.x, this.y, enemy.x, enemy.y) < 
+								getDistance(this.x, this.y, closest.x, closest.y)) {
+						closest = enemy;
+					}
+				});
+			}
+		}
+
+		return closest;
+	}
 
 	// Draw the ship
 	this.draw = function () {
@@ -180,6 +277,7 @@ function Ship (x, y, team, size, color, isPlayer) {
 	// Destroy the ship
 	this.destroy = function () {
 		ships.splice(ships.indexOf(this), 1);
+		teams[team].splice(teams[team].indexOf(this), 1);
 	};
 }
 
@@ -351,3 +449,47 @@ function Input () {
 		}
 	});
 }
+
+
+/*************************
+* Helper functions
+**************************/
+
+// Finds the distance between two points
+function getDistance (x1, y1, x2, y2) {
+	return Math.sqrt( Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2) ) ;
+}
+
+// Finds the angle between two points
+function getAngle (x1, y1, x2, y2) {
+	var deltaY = y2 - y1;
+	var deltaX = x2 - x1;
+
+	var angle = Math.atan2(deltaY, deltaX) * 180/ Math.PI;
+
+	if (angle >= 360) {
+		angle = 360 - angle;
+	}else if (angle < 0) {
+		angle = 360 + angle;
+	}
+
+	return angle;
+}
+
+
+// Finds the sign of a number
+function sign(x) {
+	return typeof x === 'number' ? x ? x < 0 ? -1 : 1 : x === x ? 0 : NaN : NaN;
+}
+
+function getMousePos(canvas, evt) {
+var rect = canvas.getBoundingClientRect();
+	return {
+		x: evt.clientX - rect.left,
+		y: evt.clientY - rect.top
+	};
+}
+
+c.addEventListener('mousemove', function(evt) {
+	mousePos = getMousePos(c, evt);
+}, false);
